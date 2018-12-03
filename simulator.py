2fd93ad2bb4609ab6733861706ac5f5e011c6493
Dajun Luo <luodj@wegene.com>
2018-12-02 22:41:02 -0500

    1. migrate plot base to seaborn\n2. add transaction log and plot function\n3. change description in jupyter notebook\n4. add demo plot

--- simulator.py
+++ simulator.py
@@ -1,18 +1,23 @@
 __title__ = 'simulator'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 __author__ = 'Dajun Luo'
 
 from heapq import heappush, heappop
 from collections import defaultdict
+import time as systime
 
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+sns.set(rc={'figure.figsize': (20, 10)})
+from scipy import stats
 
 
 class Transaction(object):
     """
     Represents a single transaction.
     """
+
     def __init__(self, amount, fee, create_time):
         """
         :param amount: transaction amount
@@ -45,8 +50,10 @@ class Transcation_Pool(object):
     A pool of all transaction in the simulation system. We use a heap structure to retrieve the transaction with
     highest fee.
     """
+
     def __init__(self, K):
         self.pool = []
+        self.history = []
         self.K = K
         self.pending_transaction = 0
         self.finished_transaction = 0
@@ -67,6 +74,7 @@ class Transcation_Pool(object):
         self.pending_amount += transaction.amount
         self.pending_fee += transaction.fee
         heappush(self.pool, (-transaction.fee, transaction))
+        self.history.append(transaction)
 
     def generate_block(self, time):
         """
@@ -88,11 +96,28 @@ class Transcation_Pool(object):
             self.finished_fee += transaction.fee
             self.delay += time - transaction.create_time
 
+    def summary(self):
+        """
+        Summary report of transaction
+        :return: A pandas data frame of transaction information.
+        """
+        stats = defaultdict(list)
+        for transaction in self.history:
+            stats['amount'].append(transaction.amount)
+            stats['fee'].append(transaction.fee)
+            stats['create_time'].append(transaction.create_time)
+            stats['complete_time'].append(transaction.complete_time)
+            stats['finished'].append(transaction.complete_time != None)
+        stats = pd.DataFrame(stats)
+        stats['pending_time'] = stats['complete_time'] - stats['create_time']
+        return stats
+
 
 class BRC_log(object):
     """
     Records stats information.
     """
+
     def __init__(self):
         self.time_line = defaultdict(list)
 
@@ -104,13 +129,20 @@ class BRC_log(object):
         """
         self.time_line['time(minutes)'].append(simulator.time)
         pool = simulator.pool
-        self.time_line['all_transaction_count'].append(pool.pending_transaction + pool.finished_transaction)
-        self.time_line['pending_transaction_count'].append(pool.pending_transaction)
-        self.time_line['finished_transaction_count'].append(pool.finished_transaction)
-        self.time_line['all_transaction_amount'].append(pool.pending_amount + pool.finished_amount)
-        self.time_line['pending_transaction_amount'].append(pool.pending_amount)
-        self.time_line['finished_transaction_amount'].append(pool.finished_amount)
-        self.time_line['all_transaction_fee'].append(pool.pending_fee + pool.finished_fee)
+        self.time_line['all_transaction_count'].append(
+            pool.pending_transaction + pool.finished_transaction)
+        self.time_line['pending_transaction_count'].append(
+            pool.pending_transaction)
+        self.time_line['finished_transaction_count'].append(
+            pool.finished_transaction)
+        self.time_line['all_transaction_amount'].append(
+            pool.pending_amount + pool.finished_amount)
+        self.time_line['pending_transaction_amount'].append(
+            pool.pending_amount)
+        self.time_line['finished_transaction_amount'].append(
+            pool.finished_amount)
+        self.time_line['all_transaction_fee'].append(
+            pool.pending_fee + pool.finished_fee)
         self.time_line['pending_transaction_fee'].append(pool.pending_fee)
         self.time_line['finished_transaction_fee'].append(pool.finished_fee)
         self.time_line['delay'].append(pool.delay)
@@ -122,18 +154,26 @@ class BRC_log(object):
         :return: A pandas data frame of stats information.
         """
         data = pd.DataFrame(self.time_line)
-        data['average_delay'] = data['delay'] / data['finished_transaction_count']
-        data['average_transaction_amount'] = data['all_transaction_amount'] / data['all_transaction_count']
-        data['average_transaction_fee'] = data['all_transaction_fee'] / data['all_transaction_count']
+        data['average_delay'] = data['delay'] / \
+            data['finished_transaction_count']
+        data['average_transaction_amount'] = data[
+            'all_transaction_amount'] / data['all_transaction_count']
+        data['average_transaction_fee'] = data[
+            'all_transaction_fee'] / data['all_transaction_count']
         data['average_pending_transaction_amount'] = data['pending_transaction_amount'] / data[
             'pending_transaction_count']
-        data['average_pending_transaction_fee'] = data['pending_transaction_fee'] / data['pending_transaction_count']
+        data['average_pending_transaction_fee'] = data[
+            'pending_transaction_fee'] / data['pending_transaction_count']
         data['average_finished_transaction_amount'] = data['finished_transaction_amount'] / data[
             'finished_transaction_count']
-        data['average_finished_transaction_fee'] = data['finished_transaction_fee'] / data['finished_transaction_count']
-        data['average_block_size'] = data['finished_transaction_count'] / data['block']
-        data['average_block_amount'] = data['finished_transaction_amount'] / data['block']
-        data['average_block_fee'] = data['finished_transaction_fee'] / data['block']
+        data['average_finished_transaction_fee'] = data[
+            'finished_transaction_fee'] / data['finished_transaction_count']
+        data['average_block_size'] = data[
+            'finished_transaction_count'] / data['block']
+        data['average_block_amount'] = data[
+            'finished_transaction_amount'] / data['block']
+        data['average_block_fee'] = data[
+            'finished_transaction_fee'] / data['block']
         return data
 
 
@@ -141,6 +181,7 @@ class Simulator(object):
     """
     Simulator of the dynamic in the DES system. We use a heap transaction to fast retrieve the next event.
     """
+
     def __init__(self, transaction_generator, mu_generator, K):
         """
         Configure the simulator
@@ -159,15 +200,17 @@ class Simulator(object):
         # Add it into the transaction pool
         self.pool.add_transaction(Transaction(amount, fee, self.time))
         # Add next transaction into the event pool
-        heappush(self.event, (self.time + inter_arrival_time, self.add_transaction_event))
+        heappush(self.event, (self.time + inter_arrival_time,
+                              self.add_transaction_event))
 
     def generate_block_event(self):
         # Generate a block
         self.pool.generate_block(self.time)
         # Add next block generation event into the event pool
-        heappush(self.event, (self.time + self.mu_generator(), self.generate_block_event))
+        heappush(self.event, (self.time + self.mu_generator(),
+                              self.generate_block_event))
 
-    def run(self, duration, warm_up=0):
+    def run(self, duration, warm_up=0, verbose=False):
         """
         Run simulation.
         :param duration: length of the simulation
@@ -175,7 +218,12 @@ class Simulator(object):
         :return: None
         """
         # Loop until there is no event
+        counter = 0
+        start_time = systime.time()
         while len(self.event) != 0:
+            counter += 1
+            if counter % 10000 == 0:
+                print('Simulated %s events at time %s...' % (counter, time))
             time, event = heappop(self.event)
             if time > duration:
                 # Stop simulation after reach the deadline
@@ -188,13 +236,19 @@ class Simulator(object):
                 self.log.snapshot(self)
         # Generate statistics
         self.stats = self.log.generate_stats()
+        self.transaction_stats = self.pool.summary()
+        elapse = systime.time() - start_time
+        print('Simulation finished')
+        print('Simulated %s events, last %.1lf seconds, average speed is %.3lf events/s' %
+              (counter, elapse, counter / elapse))
 
     def reset(self):
         """
         Reset the simulation system.
         :return: None
         """
-        self.event = [(0, self.add_transaction_event), (0, self.generate_block_event)]
+        self.event = [(0, self.add_transaction_event),
+                      (0, self.generate_block_event)]
         self.pool = Transcation_Pool(self.K)
         self.time = 0
         self.log = BRC_log()
@@ -207,9 +261,51 @@ class Simulator(object):
         :return: seaborn plot
         """
         if response not in self.stats.columns:
-            raise ValueError('Only the following variables can be plotted. %s' % str(self.stats.columns))
-        plt.figure(figsize=(20,10))
-        plt.plot(self.stats['time(minutes)'], self.stats[response])
-        plt.xlabel('time (minutes)')
-        plt.ylabel(response)
+            raise ValueError(
+                'Only the following variables can be plotted. %s' % str(self.stats.columns))
+        column = self.stats[response]
+        sns.lineplot(x='time(minutes)', y=response, data=self.stats)
 
+    def transaction_plot(self, x=None, y=None):
+        """
+        Plot transaction related variables.
+        :param x, y: if only x is specified, then plot the distribution of x, otherwise plot the scatterplot
+        :return: seaborn plot
+        """
+        if x not in self.transaction_stats.columns:
+            raise ValueError('Only the following variables can be plotted. %s' % str(
+                self.transaction_stats.columns))
+
+        if y == None:
+            sns.distplot(a=self.transaction_stats[x])
+        elif y not in self.transaction_stats.columns:
+            raise ValueError('Only the following variables can be plotted. %s' % str(
+                self.transaction_stats.columns))
+        else:
+            sns.scatterplot(x=x, y=y, hue='finished',
+                            data=self.transaction_stats)
+
+if __name__ == '__main__':
+    def transaction_generator():
+        """
+        Define a transaction generator, it should return 3 value, the transaction amount, fee 
+        and an inter-arrival time. Notice that the unit for time is minute.
+        """
+        # Uniform transaction amount between 5, 25.
+        amount = stats.uniform.rvs() * 20 + 5
+        # Binomial transaction fee of 1% * amount or 2% * amount
+        fee = amount * (stats.binom.rvs(1, 0.5) / 100 + 0.01)
+        # Exponential inter-arrival time of 2 transaction / minute
+        inter_arrival_time = stats.expon.rvs(scale=0.5)
+        return amount, fee, inter_arrival_time
+
+    def mu_generator():
+        """
+        Define a block event generator, it should return a random value of an inter-arrival time for block
+        generation.
+        """
+        # Exponential inter-arrival time of 0.5 block / minute
+        return stats.expon.rvs(scale=2)
+
+    simulator = Simulator(transaction_generator, mu_generator, K=10)
+    simulator.run(duration=5000, warm_up=1000)
